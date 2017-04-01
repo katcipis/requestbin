c1360e76ba09e6f0dbd4c8a7d1c7ae634a98f945
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-03-31 21:32:24 -0300

    Here goes nothing

---
+++ README.md
@@ -0,0 +1,21 @@
+# Request Bin
+
+Helps you debug http requests, like http://requestb.in
+
+Just run:
+
+```
+go get github.com/katcipis/requestbin
+```
+
+And:
+
+```
+requestbin
+```
+
+And point to **http://localhost:8080**. Or pass a port:
+
+```
+requestbin -p <port>
+```
---
+++ requestbin.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "p", 8080, "port to listen")
+	fmt.Printf("\nport is: %d\n\n", port)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(data))
+		w.Write(data)
+	})
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
